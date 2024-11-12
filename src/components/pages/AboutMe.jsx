b0e9cb0e247ef56f109228c43f1359f42f56ada1
rachelqrwei <rachelqingranwei@gmail.com>
2024-11-11 20:08:12 -0500

    width aboutme

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -32,24 +32,24 @@ const AboutMeModal = () => {
                     </div>
 
                     {/* introductory text box */}
-                    <div className="absolute text-xs text-[#745C4E] ml-[11.2rem] mt-[3.5rem] p-2  w-[7.8rem] h-[4.7rem] bg-[#F8E6D4] rounded-[1px]">
+                    <div className="absolute text-xs text-[#745C4E] ml-[10.8rem] mt-[3.5rem] p-2  w-[7.8rem] h-[4.7rem] bg-[#F8E6D4] rounded-[1px]">
                         Hi! I'm Rachel Wei, and I'm a CS BBA student from Toronto!
                     </div>
-                    <div className="absolute text-xs text-[#745C4E] ml-[9.8rem] mt-[17.9rem] p-2 w-[10.3rem] h-[5rem]  bg-[#F8E6D4] rounded-[1px]">
+                    <div className="absolute text-xs text-[#745C4E] ml-[9.4rem] mt-[17.9rem] p-2 w-[10.3rem] h-[5rem]  bg-[#F8E6D4] rounded-[1px]">
                         I'm passionate about combining technical skills with my artistic flair to bring ideas to life.
                     </div>
-                    <div className="absolute ml-[0.6rem] mt-[24rem] w-[7rem] h-[5.3rem] pt-2 px-3 text-xs text-[#745C4E] bg-[#F8E6D4] rounded-[1px]">
+                    <div className="absolute ml-[0.2rem] mt-[24rem] w-[7rem] h-[5.3rem] pt-2 px-3 text-xs text-[#745C4E] bg-[#F8E6D4] rounded-[1px]">
                         Thanks for checking out my website!
                     </div>
 
                     {/* image elements */}
-                    <div className={`absolute ml-[17.6rem] mt-[6.5rem] w-[2.1rem] h-[2.1rem] bg-[url('/images/aboutme-star.png')] pointer-events-none bg-center bg-contain z-50`}></div>
-                    <div className={`absolute ml-[3.4rem] mt-[2.9rem] w-[7rem] h-[7rem] bg-[url('/images/aboutme-grad.png')] ${imageBoxShadow}`}></div>
-                    <div className={`absolute ml-[1.4rem] mt-[10.8rem] w-[10rem] h-[6.3rem] bg-[url('/images/aboutme-baby.png')] ${imageBoxShadow}`}></div>
-                    <div className={`absolute ml-[12.2rem] mt-[9.1rem] w-[6.3rem] h-[7.7rem] bg-[url('/images/aboutme-art.png')] ${imageBoxShadow}`}></div>
-                    <div className={`absolute ml-[3rem] mt-[17.7rem] w-[6rem] h-[5.5rem] bg-[url('/images/aboutme-cat.png')] ${imageBoxShadow}`}></div>
-                    <div className={`absolute ml-[8.2rem] mt-[23.8rem] w-[11.5rem] h-[7rem] bg-[url('/images/aboutme-concert.png')] ${imageBoxShadow}`}></div>
-                    <div className={`absolute mt-[25.1rem] w-[1.9rem] h-[1.8rem] bg-[url('/images/aboutme-star2.png')] pointer-events-none bg-center bg-contain z-50`}></div>
+                    <div className={`absolute ml-[17.2rem] mt-[6.5rem] w-[2.1rem] h-[2.1rem] bg-[url('/images/aboutme-star.png')] pointer-events-none bg-center bg-contain z-50`}></div>
+                    <div className={`absolute ml-[3.0rem] mt-[2.9rem] w-[7rem] h-[7rem] bg-[url('/images/aboutme-grad.png')] ${imageBoxShadow}`}></div>
+                    <div className={`absolute ml-[1.0rem] mt-[10.8rem] w-[10rem] h-[6.3rem] bg-[url('/images/aboutme-baby.png')] ${imageBoxShadow}`}></div>
+                    <div className={`absolute ml-[11.8rem] mt-[9.1rem] w-[6.3rem] h-[7.7rem] bg-[url('/images/aboutme-art.png')] ${imageBoxShadow}`}></div>
+                    <div className={`absolute ml-[2.6rem] mt-[17.7rem] w-[6rem] h-[5.5rem] bg-[url('/images/aboutme-cat.png')] ${imageBoxShadow}`}></div>
+                    <div className={`absolute ml-[7.8rem] mt-[23.8rem] w-[11.5rem] h-[7rem] bg-[url('/images/aboutme-concert.png')] ${imageBoxShadow}`}></div>
+                    <div className={`absolute mt-[24.7rem] w-[1.9rem] h-[1.8rem] bg-[url('/images/aboutme-star2.png')] pointer-events-none bg-center bg-contain z-50`}></div>
 
                 </div>
             </div>
